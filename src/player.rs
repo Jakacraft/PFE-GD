684a3e9c7138a552fb7e7e5ea20eb1c247b19bc5
Jakacraft <jakeclarke814@gmail.com>
2026-06-07 18:21:50 +1000

    Attempted to make movement work w/ physics

--- src/player.rs
+++ src/player.rs
@@ -1,26 +1,57 @@
 use godot::prelude::*;
-use godot::classes::Sprite2D;
-use godot::classes::ISprite2D;
+use godot::classes::{CharacterBody3D, Input};
+use godot::classes::ICharacterBody3D;
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=CharacterBody3D)]
 struct Player {
     speed: f64,
-    angular_speed: f64,
-    base: Base<Sprite2D>
+    gravity: f32,
+    jump_height: f64,
+    base: Base<CharacterBody3D>
 }
 
 #[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
+impl ICharacterBody3D for Player {
+    fn init(base: Base<CharacterBody3D>) -> Self {
         Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
+            speed: 500.0,
+            gravity: 15.0,
+            jump_height: 50.0,
             base,
         }
     }
+
     fn physics_process(&mut self, delta: f64) {
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+        let mut velocity = self.base().get_velocity();
+
+        if !self.base().is_on_floor() {
+            velocity.y -= self.gravity * delta as f32;
+        }
+
+        velocity.x = 0.0;
+        velocity.z = 0.0;
+
+        let input = Input::singleton();
+
+        if input.is_action_pressed("Forward") {
+            velocity.z -= self.speed as f32 * delta as f32;
+        }
+        if input.is_action_pressed("Backward") {
+            velocity.z += self.speed as f32 * delta as f32;
+        }
+        if input.is_action_pressed("Left") {
+            velocity.x -= self.speed as f32 * delta as f32;
+        }
+        if input.is_action_pressed("Right") {
+            velocity.x += self.speed as f32 * delta as f32;
+        }
+        // JUMP if on the floor
+        if input.is_action_pressed("Jump") && self.base().is_on_floor() {
+            velocity.y = self.jump_height as f32;
+        }
+
+        self.base_mut().set_velocity(velocity);
+        self.base_mut().move_and_slide();
     }
 }
